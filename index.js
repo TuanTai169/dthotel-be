926db1da03df5e3c5c707989f030f79e067f8d10
NguyenTuanTai <nguyentuantai412000@gmail.com>
2022-06-09 08:15:40 +0700

    v19

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-require("dotenv").config()
-require("./config/database").connectDB()
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const path = require("path")
-const fileUpload = require("express-fileupload")
+require('dotenv').config();
+require('./config/database').connectDB();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const path = require('path');
+const fileUpload = require('express-fileupload');
 
 //Import Routes
-const routes = require("./routes")
+const routes = require('./routes');
 
 //Middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 app.use(
   fileUpload({
     useTempFiles: true,
   })
-)
-app.use(routes)
+);
+app.use(routes);
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   //set static
-  app.use(express.static(path.join(__dirname, "client", "build")))
+  app.use(express.static(path.join(__dirname, 'app', 'client', 'build')));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
-  })
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'app', 'client', 'build', 'index.html'));
+  });
 }
 
 //Listen
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server started on post ${PORT}`))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on post ${PORT}`));
