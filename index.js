42877cf7336fc3a957f0facf4f42de6c65789e0a
NguyenTuanTai <nguyentuantai412000@gmail.com>
2022-06-09 09:07:29 +0700

    v23

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ app.use(routes);
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   //set static
-  app.use(express.static('app/client/build'));
+  app.use(express.static('client/build'));
   app.use(express.static('public'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'app/client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
   });
 }
 
